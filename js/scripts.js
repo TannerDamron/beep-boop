6a392a3ac0428ab28270b383e10829875b7d14d0
Tanner Damron <tanner.mdamron@yahoo.com>
2019-01-27 13:10:08 -0800

    add img and scrollIntoView attach to submit function for img

--- js/scripts.js
+++ js/scripts.js
@@ -33,6 +33,8 @@ $(document).ready(function() {
     var outputName = stringOutput.replace(/Dave/g, name)
     $("#result").addClass('magictime vanishIn').show();
     $("#result").text(outputName);
+    $("img").addClass('magictime foolishIn').show();
+    $("#img")[0].scrollIntoView();
   });
 
   $("form#formTwo").submit(function(event) {
@@ -53,5 +55,7 @@ $(document).ready(function() {
     var reverseOutputName = reverseOutput.replace(/Dave/g, name)
     $("#result").addClass('magictime vanishIn').show();
     $("#result").text(reverseOutputName);
+    $("#img").addClass('magictime foolishIn').show();
+    $("#img")[0].scrollIntoView();
   });
 });
